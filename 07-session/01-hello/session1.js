a2bd6ac3b75a3e43f1cf221878052e885908d477
ccckmit <ccckmit@gmail.com>
2021-10-22 11:33:22 +0800

    modify session to oak_session

--- 07-session/01-hello/session1.js
+++ 07-session/01-hello/session1.js
@@ -1,31 +1,37 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { Session } from "https://deno.land/x/session@1.1.0/mod.ts";
+import { Session } from "https://deno.land/x/oak_sessions/mod.ts";
 
 const app = new Application();
 
-// Configuring Session for the Oak framework
-const session = new Session({ framework: "oak" });
-await session.init();
-
-// Adding the Session middleware. Now every context will include a property
-// called session that you can use the get and set functions on
-app.use(session.use()(session));
-
-
-// Creating a Router and using the session
+// Instantiate session
+const session = new Session();
 const router = new Router();
 
-router.get("/", async (context) => {
+// Include the session.initMiddleware on the routes you want to use sessions for
+router.get("/session", session.initMiddleware(), async (ctx) => {
 
     // Examples of getting and setting variables on a session
-    if (await context.state.session.get("pageCount") === undefined) {
-        await context.state.session.set("pageCount", 0);
+    if (!await ctx.state.session.has("pageCount")) {
+        await ctx.state.session.set("pageCount", 0);
 
     } else {
-        await context.state.session.set("pageCount", await context.state.session.get("pageCount") + 1);
+        await ctx.state.session.set("pageCount", await ctx.state.session.get("pageCount") + 1);
     }
+
+    // If you only want a variable to survive for a single request, you can "flash" it instead
+    await ctx.state.session.flash("message", "I am good for form validations errors, success messages, etc.")
     
-    context.response.body = `Visited page ${await context.state.session.get("pageCount")} times`;
+    ctx.response.body = `Visited page ${await ctx.state.session.get("pageCount")} times`;
+})
+// When deleting a session, it's not recommended to do it with the session middleware in the route you perform the deletion.
+.get('/delete', async (ctx) => {
+    console.log('delete...')
+    // Pass the router context as the parameter to deleteSession()
+    await session.deleteSession(ctx)
+    // Optionally, you can also just pass the string of the session ID in case you aren't within a routing context.
+    // await session.deleteSession(ctx.cookies.get('session'))
+
+    ctx.response.redirect('/session')
 });
 
 app.use(router.routes());
